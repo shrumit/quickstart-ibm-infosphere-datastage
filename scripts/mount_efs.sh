c59d7acc2c81e5bab6bf2671bc899f6d2436462b
Shrumit Mehta <smehta@ibm.com>
2019-07-27 21:15:36 -0400

    Clean up repo for EKS refresh

--- scripts/mount_efs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-REGION=$1
-EFSID=$2
-
-mkdir -p /efs
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${EFSID}.efs.${REGION}.amazonaws.com:/ /efs
-echo "${EFSID}.efs.${REGION}.amazonaws.com:/  /efs  nfs4  nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev  0  0" >> /etc/fstab
-mount --bind /efs /mnt
-echo '/efs  /mnt  none  bind' >> /etc/fstab
